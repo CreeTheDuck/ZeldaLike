795751521f3d61a5814ae7c03102b59006cc8d85
unknown <eneryonly@gmail.com>
2021-10-14 00:07:48 -0500

    part3

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,10 +7,12 @@ public class PlayerMovement : MonoBehaviour{
     public float speed;
     private Rigidbody2D myRigidbody;
     private Vector3 change;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start(){
         myRigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,7 +21,16 @@ public class PlayerMovement : MonoBehaviour{
         change.x = Input.GetAxisRaw("Horizontal"); //defined by defualt in unity
         change.y = Input.GetAxisRaw("Vertical"); //Raw snaps directly so it doesnt feel floaty
         //Debug.Log(change); //send change to the console so you can see what the player is doing
-        if(change != Vector3.zero) { MoveCharacter(); }
+        UpdateAnimationAndMove();
+    }
+
+    void UpdateAnimationAndMove(){
+        if (change != Vector3.zero){
+            MoveCharacter();
+            animator.SetFloat("moveX", change.x); //change the direction the player is facing
+            animator.SetFloat("moveY", change.y);  //change the direction the player is facing
+            animator.SetBool("moving", true); //when moving turn on the moving animation
+        }else { animator.SetBool("moving", false); } //When not moving stop animating movement
     }
 
     void MoveCharacter(){
