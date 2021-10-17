df377226d8a6eba40497271d298e44338a1aade2
unknown <eneryonly@gmail.com>
2021-10-17 16:39:49 -0500

    part 21

--- Assets/Scripts/Log.cs
+++ Assets/Scripts/Log.cs
@@ -30,9 +30,33 @@ public class Log : Enemy{
             // move log towards target at move speed *speed since the last frame
             if ((currentState == EnemyState.idle) || (currentState == EnemyState.walk) && (currentState != EnemyState.stagger)) {// dont move towards player when attacking or staggering
                 Vector3 temp = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+
+                changeAnim(temp - transform.position);
                 myRigidbody.MovePosition(temp);
                 ChangeState(EnemyState.walk);
+                anim.SetBool("wakeUp", true);
             }
+        }
+        else if(Vector3.Distance(target.position, transform.position) > chaseRadius) {
+            anim.SetBool("wakeUp", false);
+        }
+    }
+
+    private void SetAnimFloat(Vector2 setVector) {
+        anim.SetFloat("moveX", setVector.x);
+        anim.SetFloat("moveY", setVector.y);
+    }
+
+    private void changeAnim(Vector2 direction) {
+        //Debug.Log(direction);
+        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y)) {
+            Debug.Log("x is greater");
+            if (direction.x > 0) { SetAnimFloat(Vector2.right); }
+            if (direction.x < 0) { SetAnimFloat(Vector2.left); }
+        } else if(Mathf.Abs(direction.x) < Mathf.Abs(direction.y)) {
+            Debug.Log("y is greater");
+            if (direction.y > 0) { SetAnimFloat(Vector2.up); }
+            if (direction.y < 0) { SetAnimFloat(Vector2.down); }
         }
     }
 
