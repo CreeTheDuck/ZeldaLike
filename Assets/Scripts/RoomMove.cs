062bc3573d572c3c878d4cb2f8ed28bbfccf2f0d
unknown <eneryonly@gmail.com>
2021-10-18 14:50:02 -0500

    part 26

--- Assets/Scripts/RoomMove.cs
+++ Assets/Scripts/RoomMove.cs
@@ -24,7 +24,7 @@ public class RoomMove : MonoBehaviour{
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
-        if (other.CompareTag("Player")) { //check for the player tag and the trigger
+        if (other.CompareTag("Player") && !other.isTrigger) { //check for the player tag and the trigger
             cam.minPosition += cameraChange;
             cam.maxPosition += cameraChange; // we will need two camera changes if the room isnt of the same size
             other.transform.position += playerChange;
