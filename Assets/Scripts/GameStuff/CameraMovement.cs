ba5fcabcbf00380906ef684c9e0916d7f5a442cb
unknown <eneryonly@gmail.com>
2021-10-29 16:20:25 -0500

    part 30.1

---
+++ Assets/Scripts/GameStuff/CameraMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    public Transform target;
+    public float smoothing;
+    public Vector2 maxPosition;
+    public Vector2 minPosition;
+
+    // Start is called before the first frame update
+    void Start() {
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void LateUpdate() { // happens last after an update so it follows the player
+        if (transform.position != target.position) {
+            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+            targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x); // clamps the position between min and max bounds so camera doesnt go out of scene
+            targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
+
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing); 
+            // moves the position by taking its postion , the difference of the player and smoothing it out
+        }
+    }
+}
